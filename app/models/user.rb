b184b09f68d6f43ef52543e34a511047f02eb445
Guilherme Ruiz <guiiruiz@hotmail.com>
2015-10-19 20:51:18 -0200

    Implement methods to encrypt and compare passwords in User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,23 @@
 class User < ActiveRecord::Base
+  include BCrypt
+  has_secure_password
   has_many :subscriptions
   has_many :votes
   has_many :comments
   has_many :proposals
   has_many :requirement_values
   has_many :topics
+
+  def password
+      password ||= Password.new(encrypted_password)
+  end
+
+  def password=(new_password)
+    password = Password.create(new_password)
+    self.encrypted_password = password
+  end
+
+  def authenticate(password)
+    self.password == password
+  end
 end
