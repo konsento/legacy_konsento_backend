89c2b6f83d36c7f7107c679ca6985bf0e1961658
Guilherme Ruiz <guiiruiz@hotmail.com>
2015-10-20 16:50:38 -0200

    Implement api_error method in BaseController to display errors with http status code.

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,3 +1,26 @@
 class Api::V1::BaseController  < ApplicationController
 
+  def api_error(status: 500, errors: [])
+    unless Rails.env.production?
+      puts errors.full_messages if errors.respond_to? :full_messages
+    end
+    head status: status and return if errors.empty?
+
+    render json: jsonapi_format(errors).to_json, status: status
+  end
+
+  def jsonapi_format(errors)
+    return errors if errors.is_a? String
+    errors_hash = {}
+    errors.messages.each do |attribute, error|
+      array_hash = []
+      error.each do |e|
+        array_hash << {attribute: attribute, message: e}
+      end
+      errors_hash.merge!({ attribute => array_hash })
+    end
+
+    return errors_hash
+  end
+
 end
