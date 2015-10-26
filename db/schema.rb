6e5ec319987e3121a6f2dca98a29a0487c4be6b7
Guilherme Ruiz <guiiruiz@hotmail.com>
2015-10-26 16:38:48 -0200

    Add description attribute to groups

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151020220448) do
+ActiveRecord::Schema.define(version: 20151026183623) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id",    null: false
@@ -34,9 +34,10 @@ ActiveRecord::Schema.define(version: 20151020220448) do
 
   create_table "groups", force: :cascade do |t|
     t.integer  "parent_id"
-    t.string   "title",      null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "title",       null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.text     "description"
   end
 
   add_index "groups", ["parent_id"], name: "index_groups_on_parent_id"
