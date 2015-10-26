3b08398f58c7248a44a7608dfa3fcacffaf6eb62
Guilherme Ruiz <guiiruiz@hotmail.com>
2015-10-26 19:22:28 -0200

    Implement unsubscribe method

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -1,11 +1,20 @@
 class Api::V1::SubscriptionsController < Api::V1::BaseController
-  before_filter :authenticate_user!, only: [:create]
+  before_filter :authenticate_user!, only: [:create, :unsubscribe]
 
   def create
     group = Group.find(group_params[:id])
     subscription = Subscription.create(user: @current_user, group: group, role: "default")
     if subscription.persisted?
       render( json: subscription, serializer: Api::V1::SubscriptionSerializer)
+    else
+      return api_error(status: 401)
+    end
+  end
+
+  def unsubscribe
+    subscription = Subscription.find_by(user: @current_user, group: group_params[:id])
+    if subscription.destroy
+      head status:200
     else
       return api_error(status: 401)
     end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, only: [:create, :show, :update]
       resources :invitations, only: [:create]
-      resources :subscriptions, only: [:create]
+      resources :subscriptions, only: [:create] do
+        post :unsubscribe, on: :collection
+      end
       post '/login', to: 'sessions#create'
     end
   end
