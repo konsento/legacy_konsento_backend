af591c229a33756dbd8765569f69ddd22e27fb29
Guilherme Ruiz <guiiruiz@hotmail.com>
2015-10-28 19:26:17 -0200

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,7 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-ruiz = User.new
-ruiz.username = "Ruiz"
-ruiz.email = "email@email.com"
-ruiz.password = "123456"
-ruiz.save
+user = User.create(username: "Ruiz", email: "email@email.com", password: "123456")
 
-global = Group.create(parent_id: nil, title: "Global", description: "Global group.")
+group = Group.create(parent_id: nil, title: "Global", description: "Global group.")
+
+topic = Topic.create(user: user, group: group, title: "Sample Topic")
